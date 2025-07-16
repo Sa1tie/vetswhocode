11b836d3834bf94d1fbf9655fb3abd7e76ec20ae
null <135878346+Sa1tie@users.noreply.github.com>
2025-07-15 23:59:44 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openResume() {
+  window.open("RMcNulty_RES_June2025.pdf", "_blank");
+}
+
+function openLinkedIn() {
+  window.open("https://www.linkedin.com/in/roger-mcnulty-016776219/", "_blank");
+}
